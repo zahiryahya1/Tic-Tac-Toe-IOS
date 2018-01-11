1513a522eb9660c4d1eb0d27eadbddc0462b9159
Zahir Yahya <theeYahya@gmail.com>
2018-01-11 14:11:38 -0800

    Implemented an action for the buttons to switch users and image between X and O.

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	@IBAction func Action(_ sender: Any) {
-		
+	var activePlayer = 1; // cross 'X'
+	
+	
+	@IBAction func Action(_ sender: AnyObject) {
+		if ( activePlayer == 1) {
+			// displays img
+			sender.setImage(UIImage(named: "Cross.png"), for: UIControlState());
+			activePlayer = 2; // set player to player 2
+		}
+		else {
+			// displays img
+			sender.setImage(UIImage(named: "Nought.png"), for: UIControlState());
+			activePlayer = 1; // set player to player 1
+		}
 	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
